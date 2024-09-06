aec8545550a4c0998363cc0567fb0a3fcb42ac2c
chriswebdv <chriswebdv@gmail.com>
2024-09-06 17:31:54 -0600

    updated code, to learn about the different types of initialazions

--- 1.4 - assignment/main.cpp
+++ 1.4 - assignment/main.cpp
@@ -11,5 +11,15 @@ int main()
 
 	std::cout << width; // prints 7
 
-	return 0;
+        // An integer can only hold non-fractional values
+    // int w1 { 4.5 }; // compile error: list init does not allow narrowing conversion of 4.5 to 4
+    // std::cout << w1;
+    
+    int w2 = 4.5;   // compiles: copy init initializes width with 4
+    std::cout << w2;
+
+    int w3(4.5);    // compiles: direct init initializes width with 4
+    std::cout << w3;
+	
+    return 0;
 }
